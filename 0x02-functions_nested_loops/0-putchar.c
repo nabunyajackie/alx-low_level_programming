997edfb4b1a53471c16b7fe5a45289104e6d7ae9
nabunyajackie <nabunyajackie20@gmail.com>
2022-07-05 15:28:43 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+/**
+ * main - entry point
+ *
+ * Print _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int _putchar(char c);
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Testing Nested Loops
